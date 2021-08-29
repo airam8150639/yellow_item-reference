f8929f4d6bc8cc0d644733cdb6401bc750f2ef7d
mar_anisimova <mar-anisimovaa@yandex.ru>
2021-08-29 12:24:43 +0300

    Init

---
+++ yellow_item-reference.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+#include <set>
+#include <vector>
+#include <ctime>
+#include <utility>
+using namespace std;
+
+template <typename First, typename Second>
+Second& GetRefStrict(map<First, Second>& m, First key);
+
+template <typename First, typename Second> 
+Second& GetRefStrict(map<First, Second>& m, First key) {
+	try {
+		m.at(key);
+	}
+	catch (out_of_range e) {
+		throw runtime_error("runtime_error!!!");
+	}
+	return m[key];
+}
+
+/*int main() {
+	try {
+		map<double, int> m = { {1.09, 1}};
+		int& item = GetRefStrict(m, 1.19);
+		item = 2;
+		cout << m[1.09] << endl; // выведет newvalue
+	}
+	catch (runtime_error e) {
+		cout << e.what() << endl;
+	}
+
+	return 0;
+}*/
